aa98952420cad8b693cee5de5c0bfd73ee209d00
alnutile <alfrednutile@gmail.com>
2015-05-28 21:34:11 -0400

    making progress

--- tests/spec/GherkinSpec.js
+++ tests/spec/GherkinSpec.js
@@ -1,4 +1,4 @@
-describe("Gherkin Tracks Users Clicks Makes Gherkin Tests", function() {
+describe("Gherkin converter:", function() {
 
   /**
   * # Goal capture commen events
@@ -34,7 +34,10 @@ describe("Gherkin Tracks Users Clicks Makes Gherkin Tests", function() {
              outerText: "Button with ID"
            },
            path: [
-             { button: "#foo-button.btn.btn-warning" }
+             {
+               button: "#foo-button.btn.btn-warning" ,
+               id: "foo-button" ,
+             }
            ]
          }
        }
@@ -52,27 +55,80 @@ describe("Gherkin Tracks Users Clicks Makes Gherkin Tests", function() {
                  innerText: "Email address"
                }
              ]
-           }
+           },
+           path: [
+             {
+               id: "exampleInputEmail"
+             }
+           ]
+         }
+       }
+     );
+
+     eventStore.push(
+       {
+         type: "click",
+         data: {
+           target: {
+             id: "learn-more",
+             nodeName: "A",
+             innerText: "Learn more"
+           },
          }
        }
      );
 
   });
 
-  it("should convert button click to step", function(){
-      var results = converter.stepType(eventStore[0])
-      expect(results).toEqual('When I press "BUTTON"');
+  it("should convert button click to step using label", function(){
+      var results = converter.stepType(eventStore[0]);
+      expect(results).toEqual('When I press "Button with ID"');
   });
 
   //@TODO test gets id not button text
-
+  it("should get ID since no labels", function(){
+      var eventStoreUpdated = eventStore[0];
+      eventStoreUpdated.data.target.outerText = '';
+      var results = converter.stepType(eventStoreUpdated);
+      expect(results).toEqual('When I press "foo-button"');
+  });
 
   it("should convert actions to form interactions", function(){
       var results = converter.stepType(eventStore[1])
       expect(results).toEqual('When I fill in "Email address" with "foo@foo.com"');
   });
 
-  //@TODO Test Gets ID not label
+  it("should use id if not label", function(){
+      var eventStoreUpdated = eventStore[1];
+      eventStoreUpdated.data.target.labels = [];
+      console.log(eventStoreUpdated);
+      var results = converter.stepType(eventStoreUpdated)
+      expect(results).toEqual('When I fill in "exampleInputEmail" with "foo@foo.com"');
+  });
 
-  //@TODO no label no id no name use css path
+  it("should use xpath for pressing", function(){
+      var eventStoreUpdated = eventStore[1];
+      eventStoreUpdated.data.target.labels = [];
+      console.log(eventStoreUpdated);
+      var results = converter.stepType(eventStoreUpdated)
+      expect(results).toEqual('When I fill in "exampleInputEmail" with "foo@foo.com"');
+  });
+
+  it("should output the array of values", function() {
+      var results = converter.iterateOverEvents(eventStore);
+      expect(results.length).toEqual(3);
+  })
+
+  it("should write out the results in a test", function() {
+      var results     = converter.iterateOverEvents(eventStore);
+      var finalOutput = converter.outputFeature(results);
+      expect(finalOutput).toContain("When I press \"Button with ID\"");
+      expect(finalOutput).toContain("When I fill in \"Email address\" with \"foo@foo.com\"\n");
+  })
+
+  it("should turn links into a step", function() {
+      console.log(eventStore[2]);
+      var results = converter.stepType(eventStore[2]);
+      expect(results).toEqual('When I follow "Learn more"');
+  })
 });
--- tests/src/eventsConverter.js
+++ tests/src/eventsConverter.js
@@ -2,6 +2,33 @@ function EventsConverter() {
 
 }
 
+EventsConverter.prototype.outputFeature = function(steps)
+{
+  this.steps = steps;
+  var feature = '';
+
+  this.steps.forEach(function(v, i)
+  {
+    feature = feature + v + "\n";
+  });
+
+  console.log(feature);
+  return feature;
+}
+
+EventsConverter.prototype.iterateOverEvents = function(userEvents)
+{
+  var steps = [];
+  this.userEvents = userEvents;
+
+  this.userEvents.forEach(function(v, i)
+  {
+    steps.push(EventsConverter.prototype.stepType(v));
+  });
+
+  return steps;
+}
+
 EventsConverter.prototype.stepType = function(userEvent)
 {
    console.log(userEvent);
@@ -18,10 +45,20 @@ EventsConverter.prototype.stepType = function(userEvent)
      else if (this.userEvent.data.target.nodeName == "INPUT") {
        var converted = EventsConverter.prototype.input(this.userEvent);
      }
-
+     else if (this.userEvent.data.target.nodeName == "A") {
+       var converted = EventsConverter.prototype.link(this.userEvent);
+     }
    }
 
    return converted;
+}
+
+EventsConverter.prototype.link = function(userEvent)
+{
+  this.userEvent = userEvent;
+
+  var link_text =  EventsConverter.prototype.getLinkNameOrId(this.userEvent);
+  return 'When I follow "' + link_text + '"';
 }
 
 EventsConverter.prototype.button = function(userEvent)
@@ -49,32 +86,67 @@ EventsConverter.prototype.getInputValue = function(userEvent)
 
 }
 
+EventsConverter.prototype.getLinkNameOrId = function(userEvent)
+{
+  this.userEvent = userEvent;
+
+  var labelOrId = 'not found';
+
+  if(
+      this.userEvent.data.target.innerText != "undefined"
+      &&
+      this.userEvent.data.target.innerText.length > 0
+    )
+  {
+    labelOrId = this.userEvent.data.target.innerText;
+  }
+  else if( this.userEvent.data.target.id != '' ) {
+      labelOrId = this.userEvent.data.target.id;
+  }
+
+  return labelOrId;
+}
+
 EventsConverter.prototype.getInputLabelOrDomId = function(userEvent)
 {
   this.userEvent = userEvent;
 
-  if(this.userEvent.data.target.labels != "undefined")
+  var labelOrId = 'not found';
+
+  if(
+      this.userEvent.data.target.labels != "undefined"
+      &&
+      this.userEvent.data.target.labels.length > 0
+      &&
+      this.userEvent.data.target.labels[0].innerText.length > 0
+    )
   {
-    if(this.userEvent.data.target.labels.length > 0)
-    {
-      if(this.userEvent.data.target.labels[0].innerText.length > 0)
-      {
-        return this.userEvent.data.target.labels[0].innerText;
-      }
-    }
+    labelOrId = this.userEvent.data.target.labels[0].innerText;
+  }
+  else if( this.userEvent.data.path[0].id != '' ) {
+      labelOrId = this.userEvent.data.path[0].id;
   }
 
-  return 'foo';
+  return labelOrId;
 }
 
 EventsConverter.prototype.getTextOrPath = function(userEvent)
 {
   this.userEvent = userEvent;
+  var labelOrId;
 
-  if(this.userEvent.data.target.nodeName != "undefined")
+  console.log(this.userEvent.data.target.outerText.length > 0);
+  if(
+      this.userEvent.data.target.outerText != "undefined"
+      &&
+      this.userEvent.data.target.outerText.length > 0
+    )
   {
-    return this.userEvent.data.target.nodeName;
+    labelOrId = this.userEvent.data.target.outerText;
+  }
+  else if( this.userEvent.data.path[0].id != '' ) {
+      labelOrId = this.userEvent.data.path[0].id;
   }
 
-  return this.userEvent.data.path[0].button;
+  return labelOrId;
 }
